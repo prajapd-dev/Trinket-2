e37cfc9bb7e75aa602eb625ef8ccf769a0b8fc15
Disa Prajapati <prajapd22@gmail.com>
2025-11-05 14:10:53 -0500

    updates tab navigator to have working icon and booth viewability

--- mobile/components/Navigation/TabNavigator.tsx
+++ mobile/components/Navigation/TabNavigator.tsx
@@ -1,15 +1,28 @@
 // src/navigation/TabsNavigator.tsx
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { TabsParamList } from './types';
-import Account from '../Screens/Account';
-import AddItem from '../Screens/AddItem';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { TabsParamList } from "./types";
+import Account from "../Screens/Account";
+import BoothList from "../Screens/Booth/BoothList";
+import { IconButton } from "react-native-paper";
 
 const Tab = createBottomTabNavigator<TabsParamList>();
 
 export default function TabsNavigator({ session }: any) {
+  console.log("TabsNavigator: marketId: ");
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Add Item" component={AddItem} />
+      <Tab.Screen
+        name="View Booths"
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ focused }) => {
+            const iconName = focused ? "shopping" : "shopping-outline";
+            return <IconButton icon={iconName} />;
+          },
+        }}
+      >
+        {(props) => <BoothList {...props} />}
+      </Tab.Screen>
       <Tab.Screen name="Account">
         {(props) =>
           session && session.user ? (
