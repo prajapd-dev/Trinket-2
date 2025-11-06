aaee7459b5807de2f57b4b79171775f277359337
Disa Prajapati <prajapd22@gmail.com>
2025-11-06 13:35:39 -0500

    updates naming to better match db and added in axios patch call

--- mobile/api/markets.ts
+++ mobile/api/markets.ts
@@ -13,11 +13,13 @@ export const getMarkets = async (userId: number): Promise<MarketDataGet[]> => {
 
   if (!data.success) throw new Error("getMarkets: failed to fetch markets");
 
-  return data.markets.map((m) => ({
-    ...m, 
-    startdate: new Date(m.startdate),
-    enddate: new Date(m.enddate)
-  })).sort((a, b) => a.startdate.getTime() - b.startdate.getTime());
+  return data.markets
+    .map((m) => ({
+      ...m,
+      startdate: new Date(m.startdate),
+      enddate: new Date(m.enddate),
+    }))
+    .sort((a, b) => a.startdate.getTime() - b.startdate.getTime());
 };
 
 export const createMarket = async (
@@ -34,13 +36,50 @@ export const createMarket = async (
     };
     formData.append("image", file as any);
   }
-  formData.append("marketName", market.name);
-  formData.append("startDate", market.startDate.toISOString());
-  formData.append("endDate", market.endDate.toISOString());
-  formData.append("img_uri", market.imgUri ? market.imgUri : "");
+  formData.append("name", market.name);
+  formData.append("startdate", market.startdate.toISOString());
+  formData.append("enddate", market.enddate.toISOString());
+  formData.append("img_url", market.img_url ? market.img_url : "");
   return axios.post(`${API_BASE_URL}/custom_market/${user_id}`, formData, {
     headers: {
-        "Content-Type": "multipart/form-data"
-    }
+      "Content-Type": "multipart/form-data",
+    },
   });
+};
+
+export const updateMarket = async (
+  user_id: number,
+  market_uuid: number,
+  market: Partial<MarketDataPost>,
+  imgData: any | null
+) => {
+  const formData = new FormData();
+  if (imgData != null) {
+    const file = {
+      uri: imgData.assets[0].uri,
+      type: imgData.assets[0].mimeType,
+      name: imgData.assets[0].fileName,
+    };
+    formData.append("image", file as any);
+  }
+
+  for (const key in market) {
+    const value = market[key as keyof Partial<MarketDataPost>];
+    if (value instanceof Date) {
+      formData.append(key, value.toISOString());
+    } else if (value) {
+      formData.append(key, value);
+    } else {
+      // undefined behaviour .. should never reach here
+    }
+  }
+  return axios.patch(
+    `${API_BASE_URL}/custom_market/${market_uuid}/${user_id}`,
+    formData,
+    {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    }
+  );
 };
