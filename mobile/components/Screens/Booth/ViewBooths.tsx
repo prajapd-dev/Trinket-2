f7e955b2bbd2cc9e4b06b010ef7969ace7a3e966
Disa Prajapati <prajapd22@gmail.com>
2025-11-05 20:43:20 -0500

    [frontend] updated booth view name

---
+++ mobile/components/Screens/Booth/ViewBooths.tsx
@@ -0,0 +1,161 @@
+import axios from "axios";
+import { API_BASE_URL } from "../../type";
+import { Text } from "react-native-paper";
+import { StyleSheet, View, ScrollView } from "react-native";
+import { useFocusEffect } from "@react-navigation/native";
+import { useCallback, useState } from "react";
+import CustomBoothCard from "./CustomBoothCard";
+import AddBooth from "../../Buttons/AddBooth";
+import { useMarket } from "../../Contexts/MarketContext";
+import LavenderBackground from "../../LavenderBackground";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+
+type ViewBoothsFromAPI = {
+  uuid: number;
+  name: string;
+  number: number;
+  market_id: number;
+  user_id: number;
+  latitude: number;
+  longitude: number;
+};
+
+type ApiResponse = {
+  booths: ViewBoothsFromAPI[];
+  message: string;
+};
+
+export default function ViewBooths({ route, navigation }: any) {
+  const { marketEndDate, marketName, marketStartDate } = route.params;
+  const { selectedMarketId } = useMarket();
+  const [booths, setBooths] = useState<ViewBoothsFromAPI[]>([]);
+
+  const onPressAddBooth = () => {
+    return navigation.navigate("AddBooth", navigation);
+  };
+
+  const fetchBooths = async () => {
+    console.log("ViewBooths: Fetching booths from API...");
+    const { data } = await axios.get<ApiResponse>(
+      `${API_BASE_URL}/custom_booth/${selectedMarketId}/1`
+    );
+    console.log(
+      "ViewBooths: all booth data that was fetched ",
+      JSON.stringify(data)
+    );
+    data.booths.map((b) => {
+      b.latitude = Number(b.latitude);
+      b.longitude = Number(b.longitude);
+      return b;
+    });
+    setBooths(data.booths);
+  };
+
+  const onPressEditBooth = (
+    boothUuid: number,
+    boothNumber: number,
+    boothName: string,
+    lat: number | null,
+    lng: number | null
+  ) => {
+    console.log(
+      "ViewBooths: navigating to EditCustomBooth for booth: ",
+      boothUuid,
+      boothNumber,
+      boothName,
+      lat,
+      lng
+    );
+    return navigation.navigate("EditCustomBooth", {
+      boothUuid,
+      boothNameCurr: boothName,
+      boothNumberCurr: boothNumber,
+      boothLatCurr: lat,
+      boothLngCurr: lng,
+    });
+  };
+
+  const onPressDeleteBooth = (boothUuid: number) => {
+    console.log("Delete booth pressed for booth UUID: ", boothUuid);
+    // Implement delete functionality here
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchBooths(); // re-fetch whenever you navigate back here
+    }, [])
+  );
+
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.safeArea}>
+        <LavenderBackground />
+        <AddBooth onPressAddBooth={onPressAddBooth} />
+        <View style={styles.row}>
+          <Text style={styles.marketName}>{marketName}</Text>
+
+          <Text style={styles.dateRange}>
+            {marketStartDate.toDateString()} - {marketEndDate.toDateString()}
+          </Text>
+        </View>
+
+        <ScrollView>
+          {
+            /* Render booth cards here when data is fetched */
+            booths.length > 0 ? (
+              booths.map((booth) => (
+                <View key={booth.uuid} style={styles.verticallySpaced}>
+                  <CustomBoothCard
+                    key={booth.uuid}
+                    name={booth.name}
+                    number={booth.number}
+                    lat={booth.latitude}
+                    lng={booth.longitude}
+                    onEditPress={() =>
+                      onPressEditBooth(
+                        booth.uuid,
+                        booth.number,
+                        booth.name,
+                        booth.latitude,
+                        booth.longitude
+                      )
+                    }
+                    onDeletePress={() => onPressDeleteBooth(booth.uuid)}
+                  />
+                </View>
+              ))
+            ) : (
+              <Text>No Booths Found</Text>
+            )
+          }
+        </ScrollView>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  verticallySpaced: {
+    paddingTop: 4,
+    paddingBottom: 4,
+    alignSelf: "stretch",
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between", // pushes title left, dates right
+    alignItems: "center", // vertically centers them
+  },
+  marketName: {
+    fontWeight: "bold",
+    fontSize: 18,
+    color: "#4B0082",
+  },
+  dateRange: {
+    fontSize: 14,
+    color: "#555",
+  },
+  safeArea: {
+    flex: 1,
+     padding: 10,
+  },
+});
