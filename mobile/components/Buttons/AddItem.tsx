bc0a154899a139c2aa6376e069fbd1186b980d58
Disa Prajapati <prajapd22@gmail.com>
2025-11-10 00:37:07 -0500

    [frontend] add item button component

---
+++ mobile/components/Buttons/AddItem.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+import { FAB } from "react-native-paper";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+export default function AddItem({onPressAddItem}:any) {
+
+  const insets = useSafeAreaInsets(); // Get bottom safe area
+  return (
+    <FAB
+      icon="plus"
+      style={[styles.fab, { bottom: 16 + insets.bottom }]} // padding above safe area
+      onPress={onPressAddItem}
+      color="black"
+      rippleColor="lavender"
+    />
+  );
+}
+
+const BUTTON_SIZE = 64; // Always square to stay circular
+
+const styles = StyleSheet.create({
+  fab: {
+    position: "absolute",
+    right: 16,
+    width: BUTTON_SIZE,
+    height: BUTTON_SIZE,
+    borderRadius: BUTTON_SIZE / 2, // perfect circle
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+    zIndex: 9999,
+  },
+});
